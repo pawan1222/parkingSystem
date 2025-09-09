2ef22ed434965326b0a401b3716d31ce3d58918b
pawan1222 <pawansingh6388251426@gmail.com>
2025-09-09 21:17:29 +0530

    cors

--- backend/app.js
+++ backend/app.js
@@ -16,8 +16,8 @@ const __dirname = path.resolve();
 app.use(cookieParser())
 app.use(express.json())
 app.use(cors({
-    origin: "http://localhost:5173",
-    credentials:true
+    origin: process.env.CORS_ORIGIN || "http://localhost:5173",
+    credentials: true
 }));
 
 app.get('/',async(req,res) => {
